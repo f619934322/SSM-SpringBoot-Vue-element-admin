af9ca1d1dd3505546c767917e2b60674101f31c3
619934322@qq.com <polkmn7968757>
2019-01-07 03:11:26 +0800

    测试使用JSON.toJSONString

--- Appliance/src/main/java/com/appliance/controller/DemoController.java
+++ Appliance/src/main/java/com/appliance/controller/DemoController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.appliance.pojo.DemoPojo;
 import com.appliance.pojo.dto.DemoDto;
 import com.appliance.service.DemoService;
 import com.appliance.utils.ExportPOIUtils;
 import com.appliance.utils.MD5;
+
+import net.minidev.json.JSONArray;
 
 @RestController
 @RequestMapping("/demo")
@@ -35,7 +38,7 @@ public class DemoController {
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String helloWorld() {
 		// 测试MD5
-		return MD5.md5("undefined")+" Hello World!";
+		return MD5.md5("undefined") + " Hello World!";
 	}
 
 	/**
@@ -48,8 +51,14 @@ public class DemoController {
 		DemoPojo demopojo = new DemoPojo();
 		demopojo.setUser("admin");
 		demopojo.setPassword(MD5.md5("123456"));
-		// 转为json格式
-		return JSONObject.toJSONString(demopojo);
+		DemoPojo demopojo2 = new DemoPojo();
+		demopojo2.setUser("admin2");
+		demopojo2.setPassword("654321");
+		List<DemoPojo> listDemoPojo = new ArrayList<>();
+		listDemoPojo.add(demopojo);
+		listDemoPojo.add(demopojo2);
+		// 转为json格式,由于用了RestController注解，所以不一定需要toJSONString方法将对象转换为JSON，Controller层的方法返回类型可以写为对象
+		return JSON.toJSONString(listDemoPojo);//用fastjson来转换为JSON,可以转换list和object
 	}
 
 	/**
