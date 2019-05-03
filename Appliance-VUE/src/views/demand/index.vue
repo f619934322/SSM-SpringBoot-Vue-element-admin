cd29524e84acf1f9c98c0f947205a6a737b5dc07
619934322@qq.com <polkmn7968757>
2019-05-03 15:30:56 +0800

    process.env.BASE_API代替写死的127.0.0.1

--- Appliance-VUE/src/views/demand/index.vue
+++ Appliance-VUE/src/views/demand/index.vue
@@ -177,9 +177,9 @@
   </div>
 </template>
 <script>
-import { Message } from 'element-ui'
-import permission from '@/directive/permission/index.js' // 权限判断指令
-import { pagination, reviewDemand } from '@/api/demand'
+import { Message } from "element-ui";
+import permission from "@/directive/permission/index.js"; // 权限判断指令
+import { pagination, reviewDemand } from "@/api/demand";
 const demandObj = {
   // 插入更新等对象在这初始化
   id: null,
@@ -192,7 +192,7 @@ const demandObj = {
   addedFlag: null,
   commit: null,
   reviewCommit: null
-}
+};
 export default {
   directives: { permission }, // 按钮权限判断，不符合权限的不显示按钮
   data() {
@@ -204,11 +204,11 @@ export default {
       demandObj: Object.assign({}, demandObj), // 这是审核用对象
       demandStatus: null, // 审核弹窗审核状态单选
       itemStatusList: [
-        { key: 0, status: 0, statusName: '未审核' },
-        { key: 1, status: 1, statusName: '驳回' },
-        { key: 2, status: 2, statusName: '审核但未采购' },
-        { key: 3, status: 3, statusName: '采购失败' },
-        { key: 4, status: 4, statusName: '采购成功' }
+        { key: 0, status: 0, statusName: "未审核" },
+        { key: 1, status: 1, statusName: "驳回" },
+        { key: 2, status: 2, statusName: "审核但未采购" },
+        { key: 3, status: 3, statusName: "采购失败" },
+        { key: 4, status: 4, statusName: "采购成功" }
       ], // 这是下拉框选项的审核状态,label绑定statusName，在下拉框中显示中文状态名称
       totalCount: 0,
       pagesize: 10,
@@ -222,13 +222,13 @@ export default {
       // 审核表单逻辑验证
       editRule: {
         status: [
-          { required: true, message: '请选择审核状态', trigger: 'blur' }
+          { required: true, message: "请选择审核状态", trigger: "blur" }
         ],
         itemCount: [
           {
             required: true,
-            message: '请输入物品数量（如果驳回，请输入0）',
-            trigger: 'blur'
+            message: "请输入物品数量（如果驳回，请输入0）",
+            trigger: "blur"
           },
           {
             validator(rule, value, callback) {
@@ -238,49 +238,49 @@ export default {
                 Number(value) > 0 &&
                 Number(value) < 999
               ) {
-                callback()
+                callback();
               } else {
-                callback(new Error('请输入1-999的正整数'))
+                callback(new Error("请输入1-999的正整数"));
               }
             },
-            trigger: 'blur'
+            trigger: "blur"
           }
         ],
         purchasePrice: [
           {
             required: true,
-            message: '请输入采购价格（如果驳回，请输入0）',
-            trigger: 'blur'
+            message: "请输入采购价格（如果驳回，请输入0）",
+            trigger: "blur"
           },
           {
             validator(rule, value, callback) {
-              var reg = /^-?\d{1,5}(?:\.\d{1,3})?$/
+              var reg = /^-?\d{1,5}(?:\.\d{1,3})?$/;
               if (reg.test(value)) {
-                callback()
+                callback();
               } else {
-                callback(new Error('请输入大于零小于十万不超过三位小数的数字'))
+                callback(new Error("请输入大于零小于十万不超过三位小数的数字"));
               }
             },
-            trigger: 'blur'
+            trigger: "blur"
           }
         ]
       }
-    }
+    };
   },
   created() {
     // 这里是设置打开页面自动会调用的方法
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     // 每页显示数据量变更
     handleSizeChange(val) {
-      this.pagesize = val
-      this.fetchData() // 每次选择一页显示几条的时候调用fetchData方法
+      this.pagesize = val;
+      this.fetchData(); // 每次选择一页显示几条的时候调用fetchData方法
     },
     // 页码变更
     handleCurrentChange(val) {
-      this.currentPage = val
-      this.fetchData() // 每次切换页码的时候调用fetchData方法
+      this.currentPage = val;
+      this.fetchData(); // 每次切换页码的时候调用fetchData方法
     },
     // 清空搜索选项
     clearSearchOptions() {
@@ -289,48 +289,48 @@ export default {
         itemName: null,
         createTimeBeginToEnd: [], // 这是时间的数组
         status: null // 这是下拉框的审核状态
-      }
+      };
     },
     // 带检索条件去查询列表（带检索用参数）
     searchData() {
-      this.currentPage = 1
-      this.listLoading = true
-      this.fetchData() // 跳回第一页，带条件参数去后端查询列表数据
+      this.currentPage = 1;
+      this.listLoading = true;
+      this.fetchData(); // 跳回第一页，带条件参数去后端查询列表数据
     },
     // excel导出按钮方法
     excelClick() {
-      this.dialogVisibleExeclForDemand = true
+      this.dialogVisibleExeclForDemand = true;
     },
     execlForDemand() {
-      const status = (this.demandObj.status = 2)
-      const baseURL = 'http://127.0.0.1:8011/'
+      const status = (this.demandObj.status = 2);
+      const baseURL = process.env.BASE_API
       // 因为跨域，所以使用外链
       window.location.href =
-        baseURL + '/appliance/demand/excelDemand?' + 'status=' + status
-      this.demandObj = Object.assign({}, demandObj) // 重新给修改用对象赋值初始化，demandObj为全局const对象
-      this.dialogVisibleExeclForDemand = false
+        baseURL + "/appliance/demand/excelDemand?" + "status=" + status;
+      this.demandObj = Object.assign({}, demandObj); // 重新给修改用对象赋值初始化，demandObj为全局const对象
+      this.dialogVisibleExeclForDemand = false;
     },
     // 列表数据获取（默认不带检索用参数）
     fetchData() {
-      this.listLoading = false
+      this.listLoading = false;
       const listQuery = {
         pageNum: this.currentPage, // 向后端传的页码
         pageSize: this.pagesize, // 向后端传的单页条数
         itemName: this.searchOptions.itemName, // 以物品名称进行检索
         status: this.searchOptions.status, // 查出所有选择的审核状态数据
         createTimeBeginToEnd: this.searchOptions.createTimeBeginToEnd // 时间数组
-      }
-      if (listQuery.status === null || listQuery.status === '') {
+      };
+      if (listQuery.status === null || listQuery.status === "") {
         // 如果不按清空搜索选项直接打叉（clearable）会导致状态传""，所以这里做一次判断
         // 如果未选择下拉框的审核状态用于查询，也同样必须赋值给status
-        listQuery.status = -1 // 因为后端status为int，前端如果传null，到后端就会变为默认值0，这样会导致mybtis不按逻辑执行，所以这里设置为-1
+        listQuery.status = -1; // 因为后端status为int，前端如果传null，到后端就会变为默认值0，这样会导致mybtis不按逻辑执行，所以这里设置为-1
       }
       pagination(listQuery).then(response => {
-        const data = response.data.responseData
-        this.list = data.list
-        this.totalCount = data.total
-        this.listLoading = false
-      })
+        const data = response.data.responseData;
+        this.list = data.list;
+        this.totalCount = data.total;
+        this.listLoading = false;
+      });
     },
     // 打开审核窗口
     openDialogDemandReview(
@@ -345,76 +345,76 @@ export default {
       reviewCommit
     ) {
       // 这里获取列表中选中的采购需求id
-      this.demandObj.id = id // 然后赋值给对象
-      this.demandObj.inventoryId = inventoryId
-      this.demandObj.purchasePrice = purchasePrice
-      this.demandObj.addedFlag = addedFlag
-      this.demandObj.itemName = itemName
-      this.demandObj.itemType = itemType
-      this.demandObj.status = status // 获取该条的审核状态，用于判断在审核弹窗的审核状态选择的显示
-      this.demandObj.reviewCommit = reviewCommit
+      this.demandObj.id = id; // 然后赋值给对象
+      this.demandObj.inventoryId = inventoryId;
+      this.demandObj.purchasePrice = purchasePrice;
+      this.demandObj.addedFlag = addedFlag;
+      this.demandObj.itemName = itemName;
+      this.demandObj.itemType = itemType;
+      this.demandObj.status = status; // 获取该条的审核状态，用于判断在审核弹窗的审核状态选择的显示
+      this.demandObj.reviewCommit = reviewCommit;
       if (itemCount === 0) {
-        itemCount = null // 由于后端物品数量类型为int，当数据库值为null时，后端会把他转变为0，这里转回null
+        itemCount = null; // 由于后端物品数量类型为int，当数据库值为null时，后端会把他转变为0，这里转回null
       }
-      this.demandStatus = status
-      this.demandObj.itemCount = itemCount
-      this.dialogDemandReview = true
+      this.demandStatus = status;
+      this.demandObj.itemCount = itemCount;
+      this.dialogDemandReview = true;
     },
     // 审核弹窗关闭
     handleCloseReview() {
-      this.dialogDemandReview = false
-      this.$refs.reviewForm.resetFields()
+      this.dialogDemandReview = false;
+      this.$refs.reviewForm.resetFields();
     },
     // 审核
     reviewDemand(formName) {
       if (parseInt(this.demandObj.status) === parseInt(this.demandStatus)) {
         // 必须采用转换，因为两者类型可能会不一样导致业务逻辑失效
         Message({
-          message: '请选择下一状态！',
-          type: 'warning',
+          message: "请选择下一状态！",
+          type: "warning",
           duration: 5 * 1000
-        })
-        return
+        });
+        return;
       } else {
-        this.demandObj.status = this.demandStatus // 因为直接绑定this.demandObj.status会导致选择判断bug（页面展示上的），所以另外声明一个this.demandStatus来接收前端选择的状态
+        this.demandObj.status = this.demandStatus; // 因为直接绑定this.demandObj.status会导致选择判断bug（页面展示上的），所以另外声明一个this.demandStatus来接收前端选择的状态
       }
       this.$refs[formName].validate(valid => {
         if (valid) {
-          this.demandObj.status = parseInt(this.demandObj.status) // 状态码转为数字
+          this.demandObj.status = parseInt(this.demandObj.status); // 状态码转为数字
           reviewDemand(this.demandObj)
             .then(response => {
-              const data = response.data
-              this.listLoading = false
+              const data = response.data;
+              this.listLoading = false;
               if (data.statusCode === 200) {
                 Message({
-                  message: '操作成功',
-                  type: 'success',
+                  message: "操作成功",
+                  type: "success",
                   duration: 5 * 1000
-                })
-                this.$refs[formName].resetFields()
-                this.dialogDemandReview = false
-                this.demandObj = Object.assign({}, demandObj) // 重新给修改用对象赋值初始化，demandObj为全局const对象
-                this.fetchData()
+                });
+                this.$refs[formName].resetFields();
+                this.dialogDemandReview = false;
+                this.demandObj = Object.assign({}, demandObj); // 重新给修改用对象赋值初始化，demandObj为全局const对象
+                this.fetchData();
               } else {
-                this.loading = false
+                this.loading = false;
                 Message({
-                  message: '操作失败',
-                  type: 'error',
+                  message: "操作失败",
+                  type: "error",
                   duration: 5 * 1000
-                })
+                });
               }
             })
             .catch(() => {
-              this.loading = false
+              this.loading = false;
               Message({
-                message: '操作失败',
-                type: 'error',
+                message: "操作失败",
+                type: "error",
                 duration: 5 * 1000
-              })
-            })
+              });
+            });
         }
-      })
+      });
     }
   } // 这是方法末尾花括号
-}
+};
 </script>
