02a87cd26335bebfb98dd15b19773a360bc959dd
fyhz <619934322@qq.com>
2019-05-13 16:22:01 +0800

    小修改

--- Appliance-VUE/src/views/layout/components/Sidebar/SidebarItem.vue
+++ Appliance-VUE/src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,36 +1,48 @@
 <template>
   <div v-if="!item.hidden&&item.children" class="menu-wrapper">
-
-    <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+    <template
+      v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow"
+    >
       <app-link :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item v-if="onlyOneChild.meta" :icon="onlyOneChild.meta.icon||item.meta.icon" :title="generateTitle(onlyOneChild.meta.title)" />
+        <el-menu-item
+          :index="resolvePath(onlyOneChild.path)"
+          :class="{'submenu-title-noDropdown':!isNest}"
+        >
+          <item
+            v-if="onlyOneChild.meta"
+            :icon="onlyOneChild.meta.icon||item.meta.icon"
+            :title="generateTitle(onlyOneChild.meta.title)"
+          />
         </el-menu-item>
       </app-link>
     </template>
 
     <el-submenu v-else ref="submenu" :index="resolvePath(item.path)">
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta.icon" :title="generateTitle(item.meta.title)" />
+        <item v-if="item.meta" :icon="item.meta.icon" :title="generateTitle(item.meta.title)"/>
       </template>
 
-      <template v-for="child in item.children" v-if="!child.hidden">
+      <template v-for="child in item.children">
         <sidebar-item
-          v-if="child.children&&child.children.length>0"
+          v-if="child.children&&child.children.length>0&&!child.hidden"
           :is-nest="true"
           :item="child"
           :key="child.path"
           :base-path="resolvePath(child.path)"
-          class="nest-menu" />
+          class="nest-menu"
+        />
 
         <app-link v-else :to="resolvePath(child.path)" :key="child.name">
           <el-menu-item :index="resolvePath(child.path)">
-            <item v-if="child.meta" :icon="child.meta.icon" :title="generateTitle(child.meta.title)" />
+            <item
+              v-if="child.meta"
+              :icon="child.meta.icon"
+              :title="generateTitle(child.meta.title)"
+            />
           </el-menu-item>
         </app-link>
       </template>
     </el-submenu>
-
   </div>
 </template>
 
@@ -85,7 +97,7 @@ export default {
 
       // Show parent if there are no child router to display
       if (showingChildren.length === 0) {
-        this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
+        this.onlyOneChild = { ...parent, path: '', noShowingChildren: true }
         return true
       }
 
